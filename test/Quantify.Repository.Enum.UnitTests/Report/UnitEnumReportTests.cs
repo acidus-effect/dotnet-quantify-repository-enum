b408769098b8201734a7456c8b8a951f5ada8d53
Michel Gammelgaard <michel.gammelgaard@leman.com>
2020-09-04 23:44:10 +0200

    Added unit test of the enum unit report summary.

--- test/Quantify.Repository.Enum.UnitTests/Report/UnitEnumReportTests.cs
+++ test/Quantify.Repository.Enum.UnitTests/Report/UnitEnumReportTests.cs
@@ -45,5 +45,33 @@ namespace Quantify.Repository.Enum.UnitTests.Report
             Assert.AreEqual(expectedNumberOfWarnings, report.Warnings.Count);
             Assert.AreEqual(expectedNumberOfErrors, report.Errors.Count);
         }
+
+        [DataTestMethod]
+        [DataRow(false, false, false, false, false)]
+        [DataRow(true, false, false, false, false)]
+        [DataRow(false, true, false, false, false)]
+        [DataRow(false, false, true, false, false)]
+        [DataRow(false, false, false, true, false)]
+        [DataRow(false, false, false, false, true)]
+        public void WHEN_CreatingSummary_WHILE_WarningsAndErrorsAreSet_THEN_SummaryContainsWarningsAndErrors(
+            bool expectedHasValueMissingUnitAttribute,
+            bool expectedHasValueWithInvalidUnitAttribute,
+            bool expectedBaseUnitHasUnitAttribute,
+            bool expectedIsMissingBaseUnitAttribute,
+            bool expectedHasInvalidBaseUnitAttribute)
+        {
+            // Arrange
+            var report = new UnitEnumReport(expectedHasValueMissingUnitAttribute, expectedHasValueWithInvalidUnitAttribute, expectedBaseUnitHasUnitAttribute, expectedIsMissingBaseUnitAttribute, expectedHasInvalidBaseUnitAttribute);
+
+            // Act
+            var summary = report.CreateSummary();
+
+            // Assert
+            foreach (var warning in report.Warnings)
+                Assert.IsTrue(summary.Contains(warning));
+
+            foreach (var error in report.Errors)
+                Assert.IsTrue(summary.Contains(error));
+        }
     }
 }
